d48d7142a7f46631c820550c7e30f3e0fae89ec6
Nicolas <nicolaspollio97@gmail.com>
2020-12-09 17:48:14 -0300

    Correcion

--- src/main/java/com/codeoftheweb/salvo/Util/Util.java
+++ src/main/java/com/codeoftheweb/salvo/Util/Util.java
@@ -3,10 +3,18 @@ package com.codeoftheweb.salvo.Util;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Util {
 
-    static boolean isGuest(Authentication authentication) {
+    public static boolean isGuest(Authentication authentication) {
         return authentication == null || authentication instanceof AnonymousAuthenticationToken;
     }
 
+    public static Map<String, Object> makeMap(String key, Object value) {
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put(key, value);
+        return map;
+    }
 }
